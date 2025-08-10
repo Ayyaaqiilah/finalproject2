bb367845d1b170410ca6f0dd76d49f7c0a1e8e0d
Aqiilah Ayya <aqiilah@example.com>
2025-08-10 18:03:36 +0700

    Initial commit - upload automation testing framework

--- src/test/java/web/utils/WebDriverFactory.java
+++ src/test/java/web/utils/WebDriverFactory.java
@@ -1,23 +1,23 @@
 package web.utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverFactory {
+
     public static WebDriver createChrome(boolean headless) {
-        WebDriverManager.chromedriver().setup();
-
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--remote-allow-origins=*");
 
+        // Kalau headless diaktifkan (misalnya di CI)
         if (headless) {
-            options.addArguments("--headless=new");
+            options.addArguments("--headless=new"); // Chrome versi baru
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
         }
+
+        // Optional: set window size biar stabil di CI
+        options.addArguments("--window-size=1920,1080");
 
         return new ChromeDriver(options);
     }
